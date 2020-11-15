d019152606a14ad54efb2c537385c876dfbaa889
Mohamad Baalbaki <52038486+MohamadBaalbaki@users.noreply.github.com>
2020-11-15 18:50:50 +0100

    Minor refactoring

--- spotify-albumcrawler.py
+++ spotify-albumcrawler.py
@@ -10,63 +10,55 @@ from sendgrid.helpers.mail import To
 sg = sendgrid.SendGridAPIClient(os.environ.get('INSERT_SENDGRID_API_KEY'))
 
 token = util.prompt_for_user_token(
-        username="INSERT_SPOTIFY_USERNAME",
-        scope="user-follow-read",
-        client_id="INSERT_CLIENT_ID",
-        client_secret="INSERT_CLIENT_SECRET",
-        redirect_uri="INSERT_REDIRECT_URI")
+        username = "INSERT_SPOTIFY_USERNAME",
+        scope = "user-follow-read",
+        client_id = "INSERT_CLIENT_ID",
+        client_secret = "INSERT_CLIENT_SECRET",
+        redirect_uri = "INSERT_REDIRECT_URI")
 
-spotify = spotipy.Spotify(auth=token)
+spotify = spotipy.Spotify(auth = token)
 
-msg=""
+msg = ""
 
-def getAlbumsFromAPI(artistName,uri):
+def getNewReleasesOfArtist(artistName, uri):
 	global msg
-	results = spotify.artist_albums(uri, album_type=None)
+	results = spotify.artist_albums(uri, album_type = None)
 	albums = results['items']
-	if len(albums)!=0:
+	if len(albums) != 0:
 		while results['next']:
 		    results = spotify.next(results)
 		    albums.extend(results['items'])
 		for i in range(len(albums)):	
-			if albums[i]['release_date_precision']!='year':	
-				releaseAsDate=dt.strptime(albums[i]['release_date'], "%Y-%m-%d").replace(hour=0, minute=0, second=0, microsecond=0)
-				dateToday=dt.today().replace(hour=0, minute=0, second=0, microsecond=0)
-				if releaseAsDate==dateToday:
-					msg=msg+artistName+'\n'
-					msg=msg+albums[i]['name']+'\n'
-					msg=msg+albums[i]['external_urls']['spotify']+'\n'+'\n'
-					print(msg)
+			if albums[i]['release_date_precision'] != 'year':	
+				releaseAsDate = dt.strptime(albums[i]['release_date'], "%Y-%m-%d").replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+				dateToday = dt.today().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+				if releaseAsDate == dateToday:
+					msg = msg + artistName + '\n' + albums[i]['name'] + '\n' + albums[i]['external_urls']['spotify'] + '\n' + '\n'
 					break
 
 def fetchResults():
-	artistId=""
-	artistName=""
-	f = open("artists.txt", "r")
-	for line in f:
-		artistId=re.sub(r'\t###.+\n', '', line)
-		artistName=re.sub(r'.+###', '', line)
+	artistId = ""
+	artistName = ""
+	artistsFile = open("artists.txt", "r")
+	for line in artistsFile:
+		artistId = re.sub(r'\t###.+\n', '', line)
+		artistName = re.sub(r'.+###', '', line)
 		artistName = artistName[:-1] #remove \n
-		birdy_uri = 'spotify:artist:'+artistId
-		getAlbumsFromAPI(artistName,birdy_uri)
+		birdy_uri = 'spotify:artist:' + artistId
+		getNewReleasesOfArtist(artistName, birdy_uri)
 		
 def sendEmails(msg):
 	from_email = Email("INSERT_THE_SENDGRID_SENDER_EMAIL")
-	subject = 'Spotify Releases Today: '+f"{dt.now():%d/%m/%Y}"
+	subject = 'Spotify Releases Today: ' + f"{dt.now():%d/%m/%Y}"
 	to_emails = To("INSERT_EMAIL_TO_SEND_TO")
-	#to_emails=[To('INSERT_EMAIL1_TO_SEND_TO'), To('INSERT_EMAIL2_TO_SEND_TO')]
-
-	if msg!='':
-		content = Content("text/plain", msg)
-		mail = Mail(from_email, to_emails, subject , content)
-		response = sg.send(mail)
-	else:
-		content = Content("text/plain", 'There are no new releases today')
-		mail = Mail(from_email, to_emails, subject , content)
-		response = sg.send(mail)
+	#to_emails = [To('INSERT_EMAIL1_TO_SEND_TO'), To('INSERT_EMAIL2_TO_SEND_TO')]
+	
+	if not msg: msg = 'There are no new releases today'
+		
+	content = Content("text/plain", msg)
+	mail = Mail(from_email, to_emails, subject , content)
+	response = sg.send(mail)
 
 ###########################################################################################
 fetchResults()
-print(msg)
 sendEmails(msg)
-
